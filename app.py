41ab9260e82ff399818670d582b0802c90e1afef
Yasuyuki Kikuchi <kikuchi@cloudpack.jp>
2021-10-31 15:32:03 +0900

    feat: 画像のアップロード処理を追加

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename, unescape
 
 app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template(
+        "index.html",
+        list=[
+            {"name": "hoge", "value": "one"},
+            {"name": "fuga", "value": "two"},
+            {"name": "foo", "value": "three"}
+        ],
+    )
 
 
 @app.route("/login", methods=["GET"])
@@ -19,3 +32,19 @@ def post_login():
         )
     else:
         return render_template("error.html")
+
+
+@app.route("/upload", methods=["GET"])
+def get_upload():
+    return render_template("upload.html")
+
+
+@app.route("/upload", methods=["POST"])
+def post_upload():
+
+    if request.files["image"]:
+        f = request.files["image"]
+        filepath = "static/{}".format(secure_filename(f.filename))
+        f.save(filepath)
+
+        return render_template("upload_check.html", name=filepath)
